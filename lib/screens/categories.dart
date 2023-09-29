1e45309d20818cf1b35b0225bfbb1dc89e50ffa4
Shariar Hossain <shariarhossin99@gmail.com>
2023-09-29 15:51:14 +0600

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:meal/data/dummy_data.dart';
+import 'package:meal/models/category.dart';
+import 'package:meal/screens/meals.dart';
+import 'package:meal/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    //Navigator.push(context, route)
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Pick Your Category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          //availableCategories.map((category)=> CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context,category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/meals.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:meal/models/meal.dart';
+import 'package:meal/screens/meal_details.dart';
+import 'package:meal/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Text(
+          'Uh oh ... nothing here ',
+          style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          'try Selecting a different category!',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        )
+      ]),
+    );
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal){
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
