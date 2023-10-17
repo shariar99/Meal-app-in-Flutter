441c89e8a2441685cc58d06a3f72eacf958bf2aa
Shariar Hossain <shariarhossin99@gmail.com>
2023-10-17 17:39:44 +0600

    all details

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -3,10 +3,10 @@ import 'package:meal/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({
-    super.key,
+    Key? key,
     required this.meal,
     required this.onToggleFavorite,
-  });
+  }) : super(key: key);
 
   final Meal meal;
   final void Function(Meal meal) onToggleFavorite;
@@ -25,11 +25,103 @@ class MealDetailsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      meal.title,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Duration: ${meal.duration} minutes',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Ingredients:',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    ...meal.ingredients.map((ingredient) {
+                      return Text(
+                        '- $ingredient',
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(color: Colors.white),
+                      );
+                    }).toList(),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Steps:',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    const SizedBox(height: 10),
+                    ...meal.steps.asMap().entries.map((entry) {
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Step ${entry.key + 1}:',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              entry.value,
+                              textAlign: TextAlign.start,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
